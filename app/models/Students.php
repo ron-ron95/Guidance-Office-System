79487dc8497144836e1df9a585750693350a2fee
Kuro Arashi <kuro_arashi101@yahoo.com>
2015-01-25 06:46:58 +0800

    modified model

--- app/models/Students.php
+++ app/models/Students.php
@@ -1,6 +1,7 @@
 <?php 
 
-
+use Phalcon\Mvc\Model\Validator\Uniqueness;
+use Phalcon\Mvc\Model\Validator\Email;
 /*students model where data of the students will be stored*/
 
 class Students extends \Phalcon\Mvc\Model{
@@ -28,6 +29,22 @@ public function initialize(){
 	$this->belongsTo("user_id","Users","id");
 
 }
+ /**
+     * Validate that emails are unique across users
+     */
+    public function validation()
+    {
+        $this->validate(new Uniqueness(array(
+            "field" => "email",
+            "message" => "The email is already registered"
+        )));
+        $this->validate(new Email(array(
+            "field"=>"email",
+            "message"=>"Email is required"
+            )));
+
+        return $this->validationHasFailed() != true;
+    }
 
 }
 
